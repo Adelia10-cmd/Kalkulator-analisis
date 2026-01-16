2ce2a231db90f6ab3199be5de190d5f0496d8e63
Adelia10-cmd <adeliafreelancer12@gmail.com>
2026-01-16 21:07:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(
 )
 
 st.title("Kalkulator Analisis Kimia")
-st.caption("Aplikasi perhitungan")
+st.caption("Aplikasi perhitungan analisis kimia")
 
 parameter = st.selectbox(
     "Pilih Parameter Analisis",
@@ -19,18 +19,18 @@ parameter = st.selectbox(
         "Kadar Gliserol Total",
         "Kadar Air",
         "Kadar Pengotor",
-        "Kadar Abu"
+        "Kadar Abu",
         "Bilangan Peroksida"
     ]
 )
 
-st.write("---")
+st.divider()
 
 # ================= FFA PALMITAT =================
 if parameter == "FFA sebagai Palmitat":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    V = st.number_input("Volume Titran (mL)", min_value=0.000)
-    N = st.number_input("Normalitas KOH/NaOH (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    V = st.number_input("Volume Titran (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas KOH/NaOH (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung FFA Palmitat"):
         if W > 0:
@@ -41,9 +41,9 @@ if parameter == "FFA sebagai Palmitat":
 
 # ================= FFA LAURAT =================
 elif parameter == "FFA sebagai Laurat":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    V = st.number_input("Volume Titran (mL)", min_value=0.000)
-    N = st.number_input("Normalitas KOH/NaOH (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    V = st.number_input("Volume Titran (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas KOH/NaOH (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung FFA Laurat"):
         if W > 0:
@@ -54,9 +54,9 @@ elif parameter == "FFA sebagai Laurat":
 
 # ================= BILANGAN ASAM =================
 elif parameter == "Bilangan Asam":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    V = st.number_input("Volume Titran (mL)", min_value=0.00)
-    N = st.number_input("Normalitas KOH (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    V = st.number_input("Volume Titran (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas KOH (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Bilangan Asam"):
         if W > 0:
@@ -67,10 +67,10 @@ elif parameter == "Bilangan Asam":
 
 # ================= BILANGAN IODIUM =================
 elif parameter == "Bilangan Iodium":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    B = st.number_input("Volume Blanko (mL)", min_value=0.00)
-    S = st.number_input("Volume Sampel (mL)", min_value=0.00)
-    N = st.number_input("Normalitas Na2S2O3 (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    B = st.number_input("Volume Blanko (mL)", min_value=0.0, format="%.3f")
+    S = st.number_input("Volume Sampel (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas Na2S2O3 (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Bilangan Iodium"):
         if W > 0:
@@ -81,10 +81,10 @@ elif parameter == "Bilangan Iodium":
 
 # ================= BILANGAN PENYABUNAN =================
 elif parameter == "Bilangan Penyabunan":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    B = st.number_input("Volume Blanko (mL)", min_value=0.00)
-    S = st.number_input("Volume Sampel (mL)", min_value=0.00)
-    N = st.number_input("Normalitas HCl (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    B = st.number_input("Volume Blanko (mL)", min_value=0.0, format="%.3f")
+    S = st.number_input("Volume Sampel (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas HCl (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Bilangan Penyabunan"):
         if W > 0:
@@ -95,10 +95,10 @@ elif parameter == "Bilangan Penyabunan":
 
 # ================= GLISEROL TOTAL =================
 elif parameter == "Kadar Gliserol Total":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    Vs = st.number_input("Volume Sampel (mL)", min_value=0.00)
-    Vb = st.number_input("Volume Blanko (mL)", min_value=0.00)
-    N = st.number_input("Normalitas (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    Vs = st.number_input("Volume Sampel (mL)", min_value=0.0, format="%.3f")
+    Vb = st.number_input("Volume Blanko (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas (N)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Gliserol Total"):
         if W > 0:
@@ -109,25 +109,25 @@ elif parameter == "Kadar Gliserol Total":
 
 # ================= KADAR AIR =================
 elif parameter == "Kadar Air":
-    W1 = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    W2 = st.number_input("Bobot Sampel + wadah awal (g)", min_value=0.0000)
-    W3 = st.number_input("Bobot Sampel + wadah akhir (g)", min_value=0.0000)
+    W1 = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    W2 = st.number_input("Bobot Sampel + Wadah Awal (g)", min_value=0.0, format="%.4f")
+    W3 = st.number_input("Bobot Sampel + Wadah Akhir (g)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Kadar Air"):
         if W1 > 0:
-            hasil = (W1 - (W3-W2) / W1) * 100
+            hasil = ((W2 - W3) / W1) * 100
             st.success(f"Kadar Air = {hasil:.2f} %")
         else:
-            st.error("Bobot awal tidak boleh 0")
+            st.error("Bobot sampel tidak boleh 0")
 
 # ================= KADAR PENGOTOR =================
 elif parameter == "Kadar Pengotor":
-    Wsk = st.number_input("Bobot Sampel + kertas saring (g)", min_value=0.0000)
-    Ws = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    Wk = st.number_input("Bobot Kertas Saring (g)", min_value=0.0000)
+    Wsk = st.number_input("Bobot Sampel + Kertas Saring (g)", min_value=0.0, format="%.4f")
+    Ws = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    Wk = st.number_input("Bobot Kertas Saring (g)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Kadar Pengotor"):
-        if Wsk > 0:
+        if Ws > 0:
             hasil = ((Wsk - Wk) / Ws) * 100
             st.success(f"Kadar Pengotor = {hasil:.2f} %")
         else:
@@ -135,9 +135,9 @@ elif parameter == "Kadar Pengotor":
 
 # ================= KADAR ABU =================
 elif parameter == "Kadar Abu":
-    Wa = st.number_input("Bobot Cawan Kosong (g)", min_value=0.0000)
-    Ws = st.number_input("Bobot Sampel (g)", min_value=0.0)
-    Wak = st.number_input("Bobot Sampel + cawan akhir (g)", min_value=0.0000)
+    Wa = st.number_input("Bobot Cawan Kosong (g)", min_value=0.0, format="%.4f")
+    Ws = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    Wak = st.number_input("Bobot Sampel + Cawan Akhir (g)", min_value=0.0, format="%.4f")
 
     if st.button("Hitung Kadar Abu"):
         if Ws > 0:
@@ -146,16 +146,16 @@ elif parameter == "Kadar Abu":
         else:
             st.error("Bobot sampel tidak boleh 0")
 
-# =============== BILANGAN PEROKSIDA =================
+# ================= BILANGAN PEROKSIDA =================
 elif parameter == "Bilangan Peroksida":
-    W = st.number_input("Bobot Sampel (g)", min_value=0.0000)
-    B = st.number_input("Volume Blanko (mL)", min_value=0.00)
-    S = st.number_input("Volume Sampel (mL)", min_value=0.00)
-    N = st.number_input("Normalitas Na2S2O3 0,01 (N)", min_value=0.0000)
+    W = st.number_input("Bobot Sampel (g)", min_value=0.0, format="%.4f")
+    B = st.number_input("Volume Blanko (mL)", min_value=0.0, format="%.3f")
+    S = st.number_input("Volume Sampel (mL)", min_value=0.0, format="%.3f")
+    N = st.number_input("Normalitas Na2S2O3 (N)", min_value=0.0, format="%.4f")
 
-    if st.button("Hitung Bilangan Iodium"):
+    if st.button("Hitung Bilangan Peroksida"):
         if W > 0:
             hasil = ((B - S) * N * 1000) / W
-            st.success(f"Bilangan Peroksida = {hasil:.2f}")
+            st.success(f"Bilangan Peroksida = {hasil:.2f} meq O2/kg")
         else:
             st.error("Bobot sampel tidak boleh 0")
